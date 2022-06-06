9e94b01554b52d318b7bf312cad4ad3950843a70
briantama87 <aldisurya87@gmail.com>
2022-06-06 11:32:12 +0700

    sudah sukses

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/counter', [ApiController::class, 'index']);
+Route::post('/counter/save', [ApiController::class, 'create']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
