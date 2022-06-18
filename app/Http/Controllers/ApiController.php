e19179644d5bec1a309e5790b02bf76112e98fc8
briantama87 <aldisurya87@gmail.com>
2022-06-18 22:32:05 +0700

    sudah sukses

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -90,46 +90,97 @@ class ApiController extends Controller
     public function upstore(Request $request)
     {
         $snfg = $request->snfg;
+        $totalreject = $request->total_reject;
+        $totallolos = $request->total_lolos;
 
-        if($snfg == 'null'){
-            return response()->json('data null tidak disimpan di database');
+        $last = Counter::latest()->first();
+        //  dd($last);
+
+        // pertama kali sistem dijalankan
+        if ($last == null) {
+            $counters = new Counter;
+            $counters->snfg = $request->snfg;
+            $counters->line = $request->line;
+            $counters->reject = 0;
+            $counters->lolos = 0;
+            $counters->total_reject = 0;
+            $counters->total_lolos = 0;
+            if ($counters->save()) {
+                return response()->json('Data berhasil disimpancukkk!');
+            } else {
+                return response()->json('error');
+            }
         } else {
-            $counter = Counter::where('snfg', '=', $snfg)->first();
-
-            /// kondisi ketika snfg sudah ada di tabel
-            if ($counter == null) {
-                $counters = New Counter;
+            // insert data jika sudah adaa
+            $snfg_last = $last->snfg;
+            $tot_reject = $last->total_reject;
+            $tot_lolos = $last->total_lolos;
+            if ($snfg_last != "null" && ($totalreject != $tot_reject || $totallolos != $tot_lolos)) {
+                $counters = new Counter;
                 $counters->snfg = $request->snfg;
                 $counters->line = $request->line;
-                $counters->lolos = $request->lolos;
-                $counters->reject = $request->reject;
-                if ($counters->save()){
-                    return response()->json('Data berhasil ditambahkan!');
+                $counters->reject = ($request->total_reject) - $tot_reject;
+                $counters->lolos = ($request->total_lolos) - $tot_lolos;
+                $counters->total_reject = $request->total_reject;
+                $counters->total_lolos = $request->total_lolos;
+                if ($counters->save()) {
+                    return response()->json('Data berhasil simpan baru');
+                } else {
+                    return response()->json('error simpan baru');
                 }
             } else {
-                // kondisi ketika snfg belum ada ditabel
-                $counter = DB::table('counters')
-                    ->select('id')
-                    ->where('snfg', $snfg)
-                    ->get();
-                foreach ($counter as $counters) {
-                }
-    
-                $counters = Counter::find($counters->id);
-                $counters->snfg = $request->snfg;
-                $counters->line = $request->line;
-                $counters->lolos = $request->lolos;
-                $counters->reject = $request->reject;
-                if ($counters->save()) {
-                    return response()->json('Data berhasil diupdate!');
-    
-                }
-    
+                return response()->json('data sudah ada');
             }
-
         }
 
-       
+
+
+
+
+
+
+
+
+
+        // if($snfg == 'null'){
+        //     return response()->json('data null tidak disimpan di database');
+        // } else {
+        //     $counter = Counter::where('snfg', '=', $snfg)->first();
+
+        //     /// kondisi ketika snfg sudah ada di tabel
+        //     if ($counter == null) {
+        //         $counters = New Counter;
+        //         $counters->snfg = $request->snfg;
+        //         $counters->line = $request->line;
+        //         $counters->lolos = $request->lolos;
+        //         $counters->reject = $request->reject;
+        //         if ($counters->save()){
+        //             return response()->json('Data berhasil ditambahkan!');
+        //         }
+        //     } else {
+        //         // kondisi ketika snfg belum ada ditabel
+        //         $counter = DB::table('counters')
+        //             ->select('id')
+        //             ->where('snfg', $snfg)
+        //             ->get();
+        //         foreach ($counter as $counters) {
+        //         }
+
+        //         $counters = Counter::find($counters->id);
+        //         $counters->snfg = $request->snfg;
+        //         $counters->line = $request->line;
+        //         $counters->lolos = $request->lolos;
+        //         $counters->reject = $request->reject;
+        //         if ($counters->save()) {
+        //             return response()->json('Data berhasil diupdate!');
+
+        //         }
+
+        //     }
+
+        // }
+
+
     }
 
     /**
